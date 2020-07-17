88fa44aacf82e27680903206f0a233db61eb097c
telkhayat <taieb.elkhayat@gmail.com>
2020-07-17 16:01:27 +0200

    eliaMonBebe devient HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+//# Coucou mon petit bebe, tu es le soleil de ma vie.
+//# Tu me manques a7biba
+//# anti batta, 9otta, habooba, amoora, 3asfoora.
+//# Je t'aime c'est tout.
+
+//# Papouni
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+           System.out.println("Hello, World");
+       }
+    }
